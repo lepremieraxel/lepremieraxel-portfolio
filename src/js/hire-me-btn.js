882d4a418aa708fdb9153a59be23e879fed046b1
Axel Marcial <axelmarcial151003@gmail.com>
2023-01-13 00:45:37 +0100

    oups

--- src/js/hire-me-btn.js
+++ src/js/hire-me-btn.js
@@ -1,5 +1,5 @@
 const hireMeBtn = document.querySelector('#hire-me-btn');
-const hireMeOption = document.querySelectorAll('#hire-option');
+const hireMeOption = document.querySelector('#hire-option');
 
 hireMeBtn.addEventListener('click', () => {
   hireMeOption.setAttribute('selected', 'selected');
