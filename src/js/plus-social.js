5a2202a7aad3e5ed07331fe302aec8b851c63fb4
Axel Marcial <axelmarcial151003@gmail.com>
2023-01-18 13:38:31 +0100

    maj

--- src/js/plus-social.js
+++ src/js/plus-social.js
@@ -3,16 +3,20 @@ const plusSocialContainer = document.querySelector('.plus-social-container');
 
 const plusSocialLinks = document.querySelectorAll('.plus-social-container a');
 
+const body = document.body;
+
 plusSocialLinks.forEach(plusSocialLink => {
   plusSocialLink.addEventListener('click', () => {
     plusSocialContainer.classList.toggle('inactive');
   if(plusSocialContainer.classList.contains('inactive')){
     plusSocialContainer.style.transform = 'rotate(90deg)';
     plusSocial.style.transform = 'translate(0) rotate(0)';
+    body.style.overflow = 'unset';
   }
   else {
     plusSocialContainer.style.transform = 'rotate(0)';
     plusSocial.style.transform = 'translate(-7px, -15px) rotate(-45deg)';
+    body.style.overflow = 'hidden';
   }
   })
 });
@@ -22,9 +26,11 @@ plusSocial.addEventListener('click', () => {
   if(plusSocialContainer.classList.contains('inactive')){
     plusSocialContainer.style.transform = 'rotate(90deg)';
     plusSocial.style.transform = 'translate(0) rotate(0)';
+    body.style.overflow = 'unset';
   }
   else {
     plusSocialContainer.style.transform = 'rotate(0)';
     plusSocial.style.transform = 'translate(-7px, -15px) rotate(-45deg)';
+    body.style.overflow = 'hidden';
   }
 })
